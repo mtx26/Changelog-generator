038cbd8766abe4c12f3506406a7f018551c23db9
mtx26 <134505471+Mtx26@users.noreply.github.com>
2024-11-24 17:00:15 +0100

    change port

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = create_app() # Créer l'application Flask
 
 # Lancer l'application Flask
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Utiliser le port défini par Render, ou 5000 par défaut
+    port = int(os.environ.get('PORT', 4000))  # Utiliser le port défini par Render, ou 5000 par défaut
     app = create_app()
-    app.run(host='3.75.158.163', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
