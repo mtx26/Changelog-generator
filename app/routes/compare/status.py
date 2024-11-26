5eec699b3c522f29cd9b0b770d4f7fe2986de286
mtx26 <134505471+Mtx26@users.noreply.github.com>
2024-11-26 12:54:43 +0100

    Added comments for better code readability

--- app/routes/compare/status.py
+++ app/routes/compare/status.py
@@ -2,9 +2,16 @@
 from app.routes.dependencies.__init__ import *
 
 
+# Comparaison des dépendances
 def status():
     last_dependencies = open_file("app/data/version/last_version.json")
     new_dependencies = open_file("app/data/version/new_version.json")
+
+    # Explication de l'indexation des dépendances par project_id
+    # old_indexed = {}
+    # for dep in old_dependencies["dependencies"]:
+        # project_id = dep["project_id"]
+        # old_indexed[project_id] = dep
 
     last_indexed = {dependency["project_id"]: dependency for dependency in last_dependencies["dependencies"]}
     new_indexed = {dependency["project_id"]: dependency for dependency in new_dependencies["dependencies"]}
@@ -12,12 +19,12 @@ def status():
     # Détection des ajouts, suppressions, et mises à jour
     added = [
         {"project_id": project_id, "new_version": new_indexed[project_id]["version_number"]}
-        for project_id in new_indexed if project_id not in last_indexed
+        for project_id in new_indexed if project_id not in last_indexed # or last_indexed[project_id]["version_number"] != new_indexed[project_id]["version_number"]
     ]
 
     removed = [
         {"project_id": project_id, "old_version": last_indexed[project_id]["version_number"]}
-        for project_id in last_indexed if project_id not in new_indexed
+        for project_id in last_indexed if project_id not in new_indexed # or last_indexed[project_id]["version_number"] != new_indexed[project_id]["version_number"]
     ]
 
     updated = [
